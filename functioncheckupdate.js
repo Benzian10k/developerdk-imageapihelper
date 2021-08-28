746a730f0676982cf8fb2106e8e169eac3216275
Durgesh Kumar <89626036+developerdk1973@users.noreply.github.com>
2021-08-28 22:54:25 +0530

    Update functioncheckupdate.js

--- functioncheckupdate.js
+++ functioncheckupdate.js
@@ -5,51 +5,51 @@ module.exports = {
 /**/
 /*checkupdate*/
 async function checkupdate() {
-	if (!require('node-fetch')) return;
-	const packageData = await require('node-fetch')(
-		`https://registry.npmjs.com/${require('./package.json').name}`
+	if (!require(`node-fetch`)) return;
+	const packageData = await require(`node-fetch`)(
+		`https://registry.npmjs.com/${require(`./package.json`).name}`
 	).then(text => text.json());
-	if (require('./package.json').version !== packageData['dist-tags'].latest) {
-		console.log('\n\n');
+	if (require(`./package.json`).version !== packageData[`dist-tags`].latest) {
+		console.log(`\n\n`);
 		console.log(
-			'\x1b[32m' + '---------------------------------------------------'
+			`\x1b[32m` + `---------------------------------------------------`
 		);
 		console.log(
 			`\x1b[32m` +
-				`| @ ${require('./package.json').name}                        - [] X |`
+				`| @ ${require(`./package.json`).name}                        - [] X |`
 		);
 		console.log(
-			'\x1b[32m' + '---------------------------------------------------'
+			`\x1b[32m` + `---------------------------------------------------`
 		);
 		console.log(
-			'\x1b[33m' +
+			`\x1b[33m` +
 				`|            The module is\x1b[31m out of date!\x1b[33m           |`
 		);
 		console.log(
-			'\x1b[35m' + '|             New version is available!           |'
+			`\x1b[35m` + `|             New version is available!           |`
 		);
 		console.log(
 			\x1b[34m` +
-				`|                  ${require('./package.json').version} --> ${
-					packageData['dist-tags'].latest
+				`|                  ${require(`./package.json`).version} --> ${
+					packageData[`dist-tags`].latest
 				}                |`
 		);
 		console.log(
-			`\x1b[36m` + `|        Run "npm i ${require('./package.json').name}@latest"       |`
+			`\x1b[36m` + `|        Run "npm i ${require(`./package.json`).name}@latest"       |`
 		);
 		console.log(
-			'\x1b[36m' + '|                    to update!                   |'
+			`\x1b[36m` + `|                    to update!                   |`
 		);
 		console.log(
 			`\x1b[37m` + `|          View the full changelog here:          |`
 		);
 		console.log(
-			`\x1b[31m` + `https://www.npmjs.com/package/${require('./package.json').name}`
+			`\x1b[31m` + `https://www.npmjs.com/package/${require(`./package.json`).name}`
 		);
 		console.log(
-			'\x1b[32m' + '---------------------------------------------------\x1b[37m'
+			`\x1b[32m` + `---------------------------------------------------\x1b[37m`
 		);
-		console.log('\n\n');
+		console.log(`\n\n`);
 	}
 }
 /**/
