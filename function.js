1161bf9e47c438d60d1cd02aacbafee726be602b
Durgesh Kumar <89626036+developerdk1973@users.noreply.github.com>
2021-08-28 13:25:39 +0530

    api helper

---
+++ function.js
@@ -0,0 +1,53 @@
+/*module exports*/
+module.exports = {
+	checkupdate
+};
+/**/
+/*update checker*/
+async function checkupdate() {
+	if (!require('node-fetch')) return;
+	const packageData = await require('node-fetch')(
+		`https://registry.npmjs.com/developerdk-imageapihelper`
+	).then(text => text.json());
+	if (require('./package.json').version !== packageData['dist-tags'].latest) {
+		console.log('\n\n');
+		console.log(
+			'\x1b[32m' + '---------------------------------------------------'
+		);
+		console.log(
+			'\x1b[32m' +
+				'| @ developerdk-imageapihelper                        - [] X |'
+		);
+		console.log(
+			'\x1b[32m' + '---------------------------------------------------'
+		);
+		console.log(
+			'\x1b[33m' +
+				`|            The module is\x1b[31m out of date!\x1b[33m           |`
+		);
+		console.log(
+			'\x1b[35m' + '|             New version is available!           |'
+		);
+		console.log(
+			'\x1b[34m' +
+				`|                  ${require('./package.json').version} --> ${
+					packageData['dist-tags'].latest
+				}                |`
+		);
+		console.log(
+			'\x1b[36m' +
+				'|        Run "npm i developerdk-imageapihelper@latest"       |'
+		);
+		console.log(
+			'\x1b[36m' + '|                    to update!                   |'
+		);
+		console.log(
+			'\x1b[37m' + `|          View the full changelog here:          |`
+		);
+		console.log(
+			'\x1b[32m' + '---------------------------------------------------\x1b[37m'
+		);
+		console.log('\n\n');
+	}
+}
+/**/
